1bc363593b18c991b6e289b419b3dc4837eadba9
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-18 14:18:04 -0300

    :sparkles: feat: Adicionado main router e cors na configuração da aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
-import * as express from 'express';
+import express from 'express';
+import userRouter from './routes/user.routes';
+import cors from 'cors';
 
 class App {
   public app: express.Express;
@@ -7,18 +9,27 @@ class App {
     this.app = express();
 
     this.config();
+    this.router();
 
-    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.get('/', (req, res) => res.json({ api: true, on: true }));
+  }
+
+  private router(): void {
+    this.app.use(
+      'api/v1',
+      userRouter
+    );
   }
 
   private config():void {
     const accessControl: express.RequestHandler = (_req, res, next) => {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT');
       res.header('Access-Control-Allow-Headers', '*');
       next();
     };
 
+    this.app.use(cors());
     this.app.use(express.json());
     this.app.use(accessControl);
   }
