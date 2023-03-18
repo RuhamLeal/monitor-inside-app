3207266911d391faf0a050e9233b3f5f01ddcc00
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-18 20:10:30 -0300

    :sparkles: feat: Adicionando encriptação hash para as passwords no banco de dados

--- backend/src/services/user.services.ts
+++ backend/src/services/user.services.ts
@@ -1,4 +1,5 @@
 import prisma from "../models/prismaClientConnection";
+import bcrypt from 'bcryptjs';
 import User from "../Domains/User";
 import IUser, { Admin, Login } from "../Interfaces/IUser";
 import UserValidation from "./validations/UserValidation";
@@ -22,6 +23,8 @@ export default class UserService {
       throw new UnauthorizedException('Usuario não autorizado');
     }
 
+    newUser.password = bcrypt.hashSync(newUser.password, 10);
+
     const user = await this.mysql_db.user.create({ data: newUser });
 
     return this.createNewUserDomain(user);
@@ -30,11 +33,11 @@ export default class UserService {
   public async login(loginData: Login) {
     UserValidation.validateLogin(loginData);
 
-    const {email, password} = loginData;
+    const { email, password } = loginData;
 
-    const user = await this.mysql_db.user.findFirst({ where: { email, password } });
+    const user = await this.mysql_db.user.findFirst({ where: { email } });
 
-    if (!user) {
+    if (!user || !bcrypt.compareSync(password, user.password)) {
       throw new NotFoundException('Email ou senha incorretos');
     }
 
