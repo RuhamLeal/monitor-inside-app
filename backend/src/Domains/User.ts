42229321a5418b27d5de515e84616df7087cd65a
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-18 13:25:39 -0300

    :sparkles: feat: Adicionando service para o dominio do 'User'

--- backend/src/Domains/User.ts
+++ backend/src/Domains/User.ts
@@ -4,14 +4,12 @@ class User {
   private id?: number;
   private email: string;
   private name: string;
-  private password: string;
   private admin: boolean;
 
   constructor(user: IUser) {
     this.id = user.id;
     this.email = user.email;
     this.name = user.name;
-    this.password = user.password;
     this.admin = user.admin;
   }
   
@@ -23,9 +21,6 @@ class User {
 
   public setName(name: string) { this.name = name; }
   public getName(name: string) { return name; }
-
-  public setPassword(password: string) { this.password = password; }
-  public getPassword(password: string) { return password; }
 
   public setAdmin(admin: boolean) { this.admin = admin; }
   public getAdmin(admin: boolean) { return admin; } 
---
+++ backend/src/services/user.services.ts
@@ -0,0 +1,25 @@
+import prisma from "../models/prismaClientConnection";
+import User from "../Domains/User";
+import IUser from "../Interfaces/IUser";
+import UserValidation from "./validations/UserValidation";
+import { report } from "process";
+
+export default class UserService {
+  private createNewUserDomain(newUser: IUser | null): User | null {
+    if (newUser) return new User(newUser);
+  
+    return null;
+  }
+
+  constructor(private mysql_db = prisma) {}
+
+  public async registerNewUser(newUser: IUser) {
+    UserValidation.validateSignin(newUser);
+
+    const user = await this.mysql_db.user.create({ data: newUser });
+
+    console.log(user);
+
+    return this.createNewUserDomain(user);
+  }
+}
