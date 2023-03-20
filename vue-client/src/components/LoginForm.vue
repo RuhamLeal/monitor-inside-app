a62448188c736615a4a70828ed5aefbadedde27b
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-20 09:01:58 -0300

    :recycle: refact: Retirando arquivos/pastas não utilizadas

---
+++ vue-client/src/components/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form action="">
+    <label for="login-user-input">
+      <input id="login-user-input" type="text" placeholder="User">
+    </label>
+  </form>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+
+})
+export default class LoginForm extends Vue {}
+</script>
---
+++ vue-client/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <LoginForm/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import LoginForm from '@/components/LoginForm.vue';
+
+@Options({
+  components: {
+    LoginForm,
+  },
+})
+export default class LoginView extends Vue {}
+</script>
