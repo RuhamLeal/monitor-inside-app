abb0ac7d8589ec3cb3743a89677b8dc4e73fb4f7
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-21 00:58:01 -0300

    :sparkles: feat: Adicionando mutations para pegar as notificações

--- vue-client/src/store/actions-types.ts
+++ vue-client/src/store/actions-types.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const LOGIN = 'LOGIN';
+export const NOTIFICAR = 'NOTIFICAR';
--- vue-client/src/store/index.ts
+++ vue-client/src/store/index.ts
@@ -1,15 +1,26 @@
 import { createStore, Store, useStore as vuexUseStore } from 'vuex';
 import { InjectionKey } from 'vue';
 import { State } from '@/interfaces/store';
+import { INotificacao } from '@/interfaces/INotificaçao';
 import loginModule from './modules/login';
-// import httpClient from '@/services/http';
-// import { LOGIN } from './actions-types';
+import { NOTIFICAR } from './actions-types';
 
 export const key: InjectionKey<Store<State>> = Symbol('store key');
 
 export const store = createStore<State>({
   state: {
-    value: null,
+    notificacoes: [],
+  },
+  mutations: {
+    [NOTIFICAR](state, novaNotificacao: INotificacao) {
+      novaNotificacao.id = new Date().getTime();
+      state.notificacoes.push(novaNotificacao);
+
+      setTimeout(() => {
+        state.notificacoes = state.notificacoes
+          .filter((notificacao) => notificacao.id !== novaNotificacao.id);
+      }, 4000);
+    },
   },
   modules: {
     loginModule,
