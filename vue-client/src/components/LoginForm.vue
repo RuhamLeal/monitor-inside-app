f5f0019a6c3206400dafdb84d660d0f224936cf0
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-21 00:59:42 -0300

    :sparkles: feat: Adiciona form do login

--- vue-client/src/components/LoginForm.vue
+++ vue-client/src/components/LoginForm.vue
@@ -1,16 +1,73 @@
 <template>
-  <form action="">
-    <label for="login-user-input">
-      <input id="login-user-input" type="text" placeholder="User">
-    </label>
-  </form>
+  <main>
+    <form @submit.prevent="login">
+      <section class="fields">
+        <label for="login-input-email" class="label">
+          Usuario
+          <input
+            type="text"
+            class="login-input"
+            v-model="inputEmail"
+            id="login-input-email"
+          />
+        </label>
+        <label for="login-input-password" class="label">
+          Senha
+          <input
+            type="password"
+            class="login-input"
+            v-model="inputPassword"
+            id="login-input-password"
+          />
+        </label>
+      </section>
+      <div class="fields">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </main>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useStore } from '@/store';
+import { LOGIN } from '@/store/actions-types';
+import useNotificador from '@/hooks/notificador';
+import { TipoNotificacao } from '@/interfaces/INotificaçao';
 
-@Options({
+export default defineComponent({
+  name: 'login-form',
+  setup() {
+    const router = useRouter();
+    const store = useStore();
 
-})
-export default class LoginForm extends Vue {}
+    const { notificar } = useNotificador();
+
+    const inputEmail = ref('');
+    const inputPassword = ref('');
+
+    const login = async () => {
+      const res = await store.dispatch(
+        LOGIN,
+        { email: inputEmail.value, password: inputPassword.value },
+      );
+
+      if (res.message) {
+        return notificar(TipoNotificacao.FALHA, 'Erro ao logar', res.message);
+      }
+
+      localStorage.setItem('token', res.token);
+
+      return router.push('/');
+    };
+
+    return {
+      inputEmail,
+      inputPassword,
+      login,
+      notificar,
+    };
+  },
+});
 </script>
--- vue-client/src/views/LoginView.vue
+++ vue-client/src/views/LoginView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <LoginForm/>
+    <Notificaçao />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import LoginForm from '@/components/LoginForm.vue';
+import Notificaçao from '@/components/Notificação.vue';
 
 @Options({
   components: {
     LoginForm,
+    Notificaçao,
   },
 })
 export default class LoginView extends Vue {}
