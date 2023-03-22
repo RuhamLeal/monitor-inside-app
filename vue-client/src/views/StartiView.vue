4b7e3e86120fa943d53005febd31a8fd27dba3ab
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-21 21:18:41 -0300

    :lipstick: style: Adicionando estilização da main page

--- vue-client/src/views/StartiView.vue
+++ vue-client/src/views/StartiView.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="starti-main">
+  <div id="starti-container">
     <Sidebar />
-    <router-view/>
+    <main id="starti-main">
+      <Header />
+      <router-view id="starti-routes"/>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Sidebar from '@/components/Sidebar.vue';
+import Header from '@/components/Header.vue';
 
 @Options({
   components: {
     Sidebar,
+    Header,
   },
 })
 export default class LoginView extends Vue {}
 </script>
 
 <style >
-  #starti-main {
+  #starti-container {
     display: flex;
   }
 
-  body, html, div#app, div#starti-main {
+  #starti-main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #starti-routes {
+    height: 100%;
+    background-color: rgb(233, 233, 233);
+  }
+
+  body, html, div#app, div#starti-container {
     width: 100%;
     height: 100%;
     margin: 0;
