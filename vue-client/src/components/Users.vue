8e08913728f6055f2785ae5c6e7025c763463ad6
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-22 00:38:39 -0300

    :sparkles: feat: Adicionando componente para gerenciar usuarios

---
+++ vue-client/src/components/Users.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    users
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'users-component-2',
+
+});
+</script>
