11c551a00b984bd1457f551a3d2633bb92ca187d
Ruham Leal <ruhamxlpro@hotmail.com>
2023-03-21 21:18:00 -0300

    :sparkles: feat: Adicionando componente Header

---
+++ vue-client/src/components/Header.vue
@@ -0,0 +1,22 @@
+h<template>
+  <header>
+    HEADER
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'header-component',
+
+});
+</script>
+
+<style scoped>
+  header {
+    background: #233546;
+    width: 100%;
+    height: 10%;
+  }
+</style>
